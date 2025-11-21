140332b6640f45e74127bc58c5c506aa681ee316
paulo16-bit <77926556+paulo16-bit@users.noreply.github.com>
2025-11-20 21:52:13 -0300

    Remove unnecessary line breaks in CarBuilder.java

--- AtividadeParte1/atividadeQ9/src/ex/com/atividade/CarBuilder.java
+++ AtividadeParte1/atividadeQ9/src/ex/com/atividade/CarBuilder.java
@@ -4,17 +4,15 @@ public abstract class CarBuilder {
 
     protected Car car = new Car();
 
-    // Template Method – define o processo fixo
     public final Car buildCar() {
         buildChassis();
         installEngine();
         setupFeatures();
         return car;
     }
-
-    // Métodos a serem customizados pelos builders concretos
     protected abstract void buildChassis();
     protected abstract void installEngine();
     protected abstract void setupFeatures();
 }
+
 
